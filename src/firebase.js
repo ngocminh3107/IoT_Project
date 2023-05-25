5f95b373cf78efc3562b1b6cbd81dddb928a75e1
Ngọc Minh <75270477+ngocminh3107@users.noreply.github.com>
2023-05-25 15:26:08 +0000

    config firebase

--- src/firebase.js
+++ src/firebase.js
@@ -12,14 +12,14 @@ import "firebase/compat/storage";
 // Cấu hình Firebase cho ứng dụng web của bạn
 // Đối với Firebase JS SDK v7.20.0 và mới hơn, measurementId là tùy chọn
 const firebaseConfig = {
-  apiKey: "AIzaSyCmFsd4_kEyQ_ksVgU6M51g3Cx5pilqgK4",
-  authDomain: "chimyen2-933cb.firebaseapp.com",
-  databaseURL: "https://chimyen2-933cb-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "chimyen2-933cb",
-  storageBucket: "chimyen2-933cb.appspot.com",
-  messagingSenderId: "844465133033",
-  appId: "1:844465133033:web:b0c339044defab769c6778",
-  measurementId: "G-JHZ0P574KY"
+  apiKey: "AIzaSyCQLPzYHHfPhfb_9Xlu9C3z9UTET_lvhv0",
+  authDomain: "chimyen3-55934.firebaseapp.com",
+  databaseURL: "https://chimyen3-55934-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chimyen3-55934",
+  storageBucket: "chimyen3-55934.appspot.com",
+  messagingSenderId: "235629842956",
+  appId: "1:235629842956:web:76a0df3f11c3ed7b511429",
+  measurementId: "G-25N6X1JH2Z"
 };
 
 // Khởi tạo Firebase
